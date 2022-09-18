f34ca41a85c6bb5c4e1ee3a442031d0430e2af4e
Jonathan Clem <j@jclem.me>
2022-09-18 09:58:52 -0400

    Create @jclem/assert package

---
+++ packages/assert/src/assert.ts
@@ -0,0 +1,35 @@
+/**
+ * Assert that `value` is not null or undefined.
+ *
+ * @param value The value to assert on
+ * @param message An error message thrown when `value` is null or undefined
+ * @returns The non-null-non-undefined value
+ */
+export const assert = <T>(
+  value: T | undefined | null,
+  message = `Expected value, but got none`
+): T => {
+  if (value == null) {
+    throw new Error(message)
+  }
+
+  return value
+}
+
+/**
+ * Assert that `value` is a string and return it.
+ *
+ * @param value The value to assert on, of unknown type
+ * @param message An error message thrown when `value` is not a string
+ * @returns A string
+ */
+export const assertString = (
+  value: unknown,
+  message = `Expected a string, but got ${value}`
+): string => {
+  if (typeof value !== 'string') {
+    throw new Error(message)
+  }
+
+  return value
+}
---
+++ packages/assert/test/assert.test.ts
@@ -0,0 +1,42 @@
+import {describe, expect, test} from '@jest/globals'
+import {assert, assertString} from '../src/assert'
+
+describe('assert', () => {
+  test('passes with a value', () => {
+    expect(assert('foo')).toBe('foo')
+  })
+
+  test('passes with a falsey value', () => {
+    expect(assert(false)).toBe(false)
+  })
+
+  test('raises with null', () => {
+    expect(() => assert(null)).toThrow()
+  })
+
+  test('raises with undefined', () => {
+    expect(() => assert(undefined)).toThrow()
+  })
+
+  test('accepts a custom message', () => {
+    expect(() => assert(null, 'Given null')).toThrow('Given null')
+  })
+})
+
+describe('assertString', () => {
+  test('passes with a string', () => {
+    expect(assertString('foo')).toBe('foo')
+  })
+
+  test('passes with an empty string', () => {
+    expect(assertString('')).toBe('')
+  })
+
+  test('raises with a non-string', () => {
+    expect(() => assertString(1)).toThrow()
+  })
+
+  test('accepts a custom message', () => {
+    expect(() => assertString(1, 'Given 1')).toThrow('Given 1')
+  })
+})
